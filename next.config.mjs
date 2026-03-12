5c7bd4b387b790176292c8dbdfa61c109077573e
Mahfuz1912 <smahfuz945@gmail.com>
2026-03-12 12:45:35 +0600

    fix: update channelAvatar URLs and adjust image component usage in video details page

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: "yt3.ggpht.com" },
+      { hostname: "yt3.googleusercontent.com" },
       { hostname: "i.ytimg.com" },
     ],
   },
